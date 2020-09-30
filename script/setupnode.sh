5b746c40efa7e7f4ebfbaeb66220b02395fe6ee0
root <root@OpenWrt_ea8300_living_1.lan>
2020-09-30 10:04:12 -0400

    minor

--- script/setupnode.sh
+++ script/setupnode.sh
@@ -10,6 +10,8 @@ rm script; ln -s haasmesh/script
 cp haasmesh/etc/profile haasmesh/etc/rc.local /etc/
 mkdir -p /usr/lib/lua/luci/controller/myapp; cp haasmesh/haas_mesh_tab.lua /usr/lib/lua/luci/controller/myapp/
 mkdir -p /usr/lib/lua/luci/view/myapp; cp haasmesh/monitor_tab.htm /usr/lib/lua/luci/view/myapp/
+cp haasmesh/etc/rc.button/reset haasmesh/etc/rc.button/wps /etc/rc.button/
+mkdir -p /etc/hotplug.d/button; cp haasmesh/etc/hotplug.d/button/buttons /etc/hotplug.d/button/
 cd -
 ash script/monstart.sh
 
