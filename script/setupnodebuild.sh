4d1a60c7355bcc3ea95fd1a22d1c8ec895a35365
Andy Haas <drandyhaas@gmail.com>
2020-09-30 13:42:10 -0400

    luma files

--- script/setupnodebuild.sh
+++ script/setupnodebuild.sh
@@ -1,5 +1,8 @@
 #git clone https://github.com/drandyhaas/haasmesh.git
-mkdir -p files/root; cp haasmesh/script files/root/
+mkdir -p files/etc/config
+cp haasmesh/etc/config/wireless_$1 files/etc/config/wireless
+cp haasmesh/etc/config/network_$1 files/etc/config/network
+mkdir -p files/root; cp -r haasmesh/script files/root/
 mkdir -p files/etc; cp haasmesh/etc/profile haasmesh/etc/rc.local files/etc/
 mkdir -p files/usr/lib/lua/luci/controller/myapp; cp haasmesh/haas_mesh_tab.lua files/usr/lib/lua/luci/controller/myapp/
 mkdir -p files/usr/lib/lua/luci/view/myapp; cp haasmesh/monitor_tab.htm files/usr/lib/lua/luci/view/myapp/
