c26646385fec1909056fedb7771d1f5bab1c9ce0
Andy Haas <drandyhaas@gmail.com>
2020-10-01 12:48:13 -0400

    fix mac after

--- script/rotatelumaeth.sh
+++ script/rotatelumaeth.sh
@@ -7,5 +7,10 @@ sed -i s%eth3%eth0%g network
 
 cd -
 
+batethmac=`ifconfig | grep HW | grep \`uci get network.bat_eth.ifname\` |cut -d ":" -f 3-|sed 's% %%g'`
+uci set network.bat_eth.macaddr="00:${batethmac}"
+uci changes
+uci commit
+
 /etc/init.d/network restart
 
