8cb33320af05633b864617064dd20633dae29a4d
root <root@OpenWrt_ea8300_living_1.lan>
2020-10-01 08:30:00 -0400

    colon

--- script/foreachnode.sh
+++ script/foreachnode.sh
@@ -1,3 +1,3 @@
 echo "Do \"${1}\" on each node"
-for n in `/root/script/nodes.sh`; do echo; echo $n; ssh $n "${1}"; done
+for n in `/root/script/nodes.sh`; do echo; echo ${n}:; ssh $n "${1}"; done
 echo
