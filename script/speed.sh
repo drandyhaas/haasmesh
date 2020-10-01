a86d8a9ae43361d6fcca96f9ce07c713654a38a6
root <root@OpenWrt_ea8300_living_1.lan>
2020-10-01 08:41:54 -0400

    2 dec places

--- script/speed.sh
+++ script/speed.sh
@@ -1,3 +1,3 @@
 x=`iperf3 $2 -t 1 -J -c $1 | grep bits_per_second | tail -1 | cut -f 2 -d ':' | cut -f 1 -d '.' | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//'`
 echo -n "   "
-echo "print($x/1000000)" |lua
+echo "print(string.format(\"%.2f\",$x/1000000))" |lua
