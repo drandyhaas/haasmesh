caf517f4a2994819d1b5b70e2b9904f5c53ebf3a
root <root@OpenWrt_ea8300_living_1.lan>
2020-10-05 16:49:33 -0400

    keep mac, so we can look up wifi strength

--- script/fixbattxt.sh
+++ script/fixbattxt.sh
@@ -7,16 +7,18 @@ while read p; do
   mac=`echo $p | cut -d " " -f 1`
   ip=`echo $p | cut -d " " -f 2`
   #echo $mac - $ip
-  sed -i "s%$mac%$ip%gi" /root/bat.txt 
+  sed -i "s%$mac%$mac $ip%gi" /root/bat.txt 
 done < /etc/bat-hosts
 
 while read p; do
   mac=`echo $p | cut -d " " -f 2 `
   ip=`echo "$p" | cut -d " " -f 3-4 `
   #echo $mac - $ip
-  sed -i "s%$mac%$ip%gi" /root/bat.txt
+  sed -i "s%$mac%$mac $ip%gi" /root/bat.txt
 done < /tmp/dhcp.leases
 
-sed -e 's%$%\ \\%' -i /root/bat.txt
-echo -n "var txt = '"; cat /root/bat.txt; echo "'" 
+#make it friendly for javascript
+sed -e 's%$%\ \\%' -i /root/bat.txt # add a \ at the end of each line
+echo -n "var txt = '"; cat /root/bat.txt; echo "'" # add the surrounding var text
+
 rm /root/bat.txt
