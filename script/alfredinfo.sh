92ef020b0e8fb6e52fc3906c4c9c87fd9eb23916
Andy Haas <drandyhaas@gmail.com>
2020-10-05 20:54:50 +0000

    add wifi strength to alfred 67 68 69 for mesh0 wifi1 and wifi2

--- script/alfredinfo.sh
+++ script/alfredinfo.sh
@@ -4,7 +4,10 @@ while true; do
  if [ $? -eq 0 ]; then
   echo `/root/script/myip.sh` `date` | alfred -s 65
   echo `/root/script/myip.sh` `echo qqq` `ifconfig |grep HWaddr |sed s%$%qqq%g` | alfred -s 66
- else 
+  iwinfo mesh0 assoclist 2>&1 | grep SNR | grep dBm | cut -d / -f 1 | alfred -s 67
+  iwinfo wlan1 assoclist 2>&1 | grep SNR | grep dBm | cut -d / -f 1 | alfred -s 68
+  iwinfo wlan2 assoclist 2>&1 | grep SNR | grep dBm | cut -d / -f 1 | alfred -s 69
+ else
   /root/script/monstart.sh
  fi
  sleep 30
