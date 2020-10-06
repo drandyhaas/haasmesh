ea98234ccf7c5fe5884f75f3df5aaa7d8ff511a7
root <root@OpenWrt_ea8300_living_1.lan>
2020-10-06 16:23:03 -0400

    parsing ips in javascript

--- script/makebatmap.sh
+++ script/makebatmap.sh
@@ -1,6 +1,32 @@
 
 cat /www/dottop.html > /www/dotbat.html
-/root/script/fixbattxt.sh >> /www/dotbat.html
+
+/root/script/bathosts.sh >> /www/dotbat.html
+
+# make it friendly for javascript
+# add a \ and maybe || at the end of each line
+# add the surrounding var text
+
+# batvis
+echo -n "var txt = '" >> /www/dotbat.html
+batadv-vis -f jsondoc | sed -e 's%$%\ \\%' >> /www/dotbat.html
+echo "'" >> /www/dotbat.html 
+
+# the wifi strengths
+echo -n "var txt2 = '" >> /www/dotbat.html
+echo -n `alfred -r 67` >> /www/dotbat.html
+echo "'" >> /www/dotbat.html
+
+# the bat-hosts
+echo -n "var txt3 = '" >> /www/dotbat.html
+cat /etc/bat-hosts | sed -e 's%$%\ ||\\%' >> /www/dotbat.html
+echo "'" >> /www/dotbat.html
+
+# the dhcp leases
+echo -n "var txt4 = '" >> /www/dotbat.html
+cat /tmp/dhcp.leases | sed -e 's%$%\ ||\\%' >> /www/dotbat.html
+echo "'" >> /www/dotbat.html
+
 cat /www/dotbottom.html >> /www/dotbat.html
 
 echo "<a href=\"../../../../dotbat.html\">Click to view map</a>"
