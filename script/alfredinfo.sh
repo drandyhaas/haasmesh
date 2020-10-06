66d1c4eba42bb9984639acc31596b76bd218e47c
root <root@OpenWrt_ea8300_living_1.lan>
2020-10-05 20:22:59 -0400

    push all wifi info to 67

--- script/alfredinfo.sh
+++ script/alfredinfo.sh
@@ -4,11 +4,7 @@ while true; do
  if [ $? -eq 0 ]; then
   echo `/root/script/myip.sh` `date` | alfred -s 65
   echo `/root/script/myip.sh` `echo qqq` `ifconfig |grep HWaddr |sed s%$%qqq%g` | alfred -s 66
-  iwinfo mesh0 assoclist 2>&1 | grep SNR | grep dBm | cut -d / -f 1 | alfred -s 67
-  iwinfo wlan1 assoclist 2>&1 | grep SNR | grep dBm | cut -d / -f 1 | alfred -s 68
-  iwinfo wlan2 assoclist 2>&1 | grep SNR | grep dBm | cut -d / -f 1 | alfred -s 69
-  iwinfo | grep ESSID | alfred -s 70
-  iwinfo | grep Channel | alfred -s 71
+  for w in `iwinfo |grep ESSID|cut -d " " -f 1`; do echo -n "$w = "; echo -n `iwinfo $w info | grep Channel`; echo -n " = "; echo `iwinfo $w assoclist | grep SNR | grep dBm | cut -d / -f 1`; done | alfred -s 67
  else
   /root/script/monstart.sh
  fi
