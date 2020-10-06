a04cb1f26b2367fae79fe4c808d31cb3deed7e5f
root <root@OpenWrt_ea8300_living_1.lan>
2020-10-06 18:44:24 -0400

    dont use file

--- script/internethtml.sh
+++ script/internethtml.sh
@@ -1,28 +1,33 @@
 
-echo "<table><tr> <th><h3>Google.com</h3></th> <th><h3>Amazon.com</h3></th> <th><h3>OpenWRT.org</h3></th> </tr>"
-  echo "<tr><th>"
-  ping -c 2 -W 4 google.com > /root/internet.log
-  res=`grep -c "bytes from" /root/internet.log`
-  if [ $res != 2 ]; then echo "<span class="reddot"></span>";
-  else echo "<span class="greendot"></span>"; fi
-  echo "&nbsp;&nbsp;`grep round-trip /root/internet.log | cut -d = -f 2 | cut -d / -f 2` ms"
+#this is the first row
+echo "<table><tr>   <th><h3 style="text-align:center">Google</h3></th>   <th><h3 style="text-align:center">OpenWRT</h3></th>   </tr>"
 
-  echo "</th><th>"
-  ping -c 2 -W 4 amazon.com > /root/internet.log
-  res=`grep -c "bytes from" /root/internet.log`
-  if [ $res != 2 ]; then echo "<span class="reddot"></span>"; 
-  else echo "<span class="greendot"></span>"; fi
-  echo "&nbsp;&nbsp;`grep round-trip /root/internet.log | cut -d = -f 2 | cut -d / -f 2` ms"
+#this is the second row
+echo "<tr>"
+  echo "<th>"
+  i1=`ping -c 2 -W 4 google.com `
+  #echo "<pre> $i1 </pre>"
+  res=`echo "$i1" | grep -c "2 packets received" `
+  #echo $res
+  echo "<p style=\"text-align:center\">"
+  if [ $res != 1 ]; then echo "<span class=\"reddot\"></span>";
+  else echo "<span class=\"greendot\"></span>"; fi
+  echo "<br>`echo "$i1" | grep "round-trip" | cut -d = -f 2 | cut -d / -f 2` ms"
+  echo "</th>"
 
-  echo "</th><th>"
-  ping -c 2 -W 4 openwrt.org > /root/internet.log
-  res=`grep -c "bytes from" /root/internet.log`
-  if [ $res != 2 ]; then echo "<span class="reddot"></span>";
-  else echo "<span class="greendot"></span>"; fi
-  echo "&nbsp;&nbsp;`grep round-trip /root/internet.log | cut -d = -f 2 | cut -d / -f 2` ms"
+  echo "<th>"
+  i1=`ping -c 2 -W 4 openwrt.org `
+  #echo "<pre> $i1 </pre>"
+  res=`echo "$i1" | grep -c "2 packets received" `
+  #echo $res
+  echo "<p style=\"text-align:center\">"
+  if [ $res != 1 ]; then echo "<span class=\"reddot\"></span>";
+  else echo "<span class=\"greendot\"></span>"; fi
+  echo "<br>`echo "$i1" | grep "round-trip" | cut -d = -f 2 | cut -d / -f 2` ms"
+  echo "</th>"
+echo "</tr>"
 
-  echo "</th></tr>"
+#that is the end
 echo "</table>"
 echo "Pings to each location shown"
 
-rm /root/internet.log
