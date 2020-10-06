4eff5c3d8e8bbcc628c2428c6cd5ccbbb6e95923
root <root@OpenWrt_ea8300_living_1.lan>
2020-10-05 21:28:20 -0400

    wifi strength for all nodes

--- script/setupnode.sh
+++ script/setupnode.sh
@@ -2,8 +2,8 @@ cd /root
 #git clone https://github.com/drandyhaas/haasmesh.git
 rm -rf script; ln -s haasmesh/script
 cp haasmesh/etc/profile haasmesh/etc/rc.local /etc/
-mkdir -p /usr/lib/lua/luci/controller/myapp; cp haasmesh/haas_mesh_tab.lua /usr/lib/lua/luci/controller/myapp/
-mkdir -p /usr/lib/lua/luci/view/myapp; cp haasmesh/monitor_tab.htm /usr/lib/lua/luci/view/myapp/
+mkdir -p /usr/lib/lua/luci/controller/myapp; ln -fs /root/haasmesh/haas_mesh_tab.lua  /usr/lib/lua/luci/controller/myapp/haas_mesh_tab.lua
+mkdir -p /usr/lib/lua/luci/view/myapp; ln -fs /root/haasmesh/monitor_tab.htm /usr/lib/lua/luci/view/myapp/monitor_tab.htm
 cp haasmesh/etc/rc.button/reset haasmesh/etc/rc.button/wps /etc/rc.button/
 mkdir -p /etc/hotplug.d/button; cp haasmesh/etc/hotplug.d/button/buttons /etc/hotplug.d/button/
 cp haasmesh/newhaasnode /www/cgi-bin/
