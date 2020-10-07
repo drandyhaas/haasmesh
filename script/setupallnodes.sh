f0d778e082c0a7d7a024c4df3bd508f5606e222b
root <root@OpenWrt_ea8300_living_1.lan>
2020-10-07 01:06:34 -0400

    python speedtest

--- script/setupallnodes.sh
+++ script/setupallnodes.sh
@@ -2,6 +2,7 @@ cd /root
 mv haasmesh/.git ./haasmesh.git
 for n in `/root/script/nodes.sh`; do 
 echo; echo ${n}
+if [ $n == `/root/script/myip.sh` ]; then continue; fi
 scp -r haasmesh $n: ; ssh $n "haasmesh/script/setupnode.sh"
 done
 mv haasmesh.git haasmesh/.git
--- script/speedtest.sh
+++ script/speedtest.sh
@@ -1,2 +1,9 @@
-#no progress, and remove special characters and empty lines
-ssh `/root/script/myip.sh` "/root/script/speedtest --accept-license -p no " | sed $'s/[^[:print:]\t]//g' | sed '/^$/d'
+if [ -f /usr/bin/speedtest ]; then
+speedtest --no-pre-allocate --no-upload
+speedtest --no-pre-allocate --no-download
+else
+echo "To install: (takes about 15MB, so make sure you have room!)"
+echo "opkg update"
+echo "opkg install python-pip"
+echo "pip install speedtest-cli"
+fi
