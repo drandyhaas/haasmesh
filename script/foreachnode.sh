2dd6f434eab9a70a0054d4064f95bc4b344f9721
root <root@OpenWrt_ea8300_living_1.lan>
2020-10-07 01:26:22 -0400

    for master vs node

--- script/foreachnode.sh
+++ script/foreachnode.sh
@@ -1,3 +1,9 @@
-echo "Do \"${1}\" on each node"
-for n in `/root/script/nodes.sh`; do echo; echo ${n}:; ssh $n "${1}"; done
-echo
+if [ `/root/script/myip.sh | cut -f 4 -d . ` -eq 1 ]; then
+ echo "Do \"${1}\" on each node"
+ for n in `/root/script/nodes.sh`; do echo; echo ${n}:; ssh $n "${1}"; done
+ echo
+else
+ echo "Just a node, so will do \"${1}\" on only myself"
+ ash -c $1
+ echo
+fi
