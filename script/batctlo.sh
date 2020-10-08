e026e074d243595008943c59395f835119aea7a0
root <root@OpenWrt_ea8300_living_1.lan>
2020-10-08 11:07:53 -0400

    better routing info

--- script/batctlo.sh
+++ script/batctlo.sh
@@ -1,3 +1,6 @@
-echo "Originator   last-seen  (#/255)   Nexthop     [outgoingIF] "
-batctl o | sed s%.-192\.168\.%192\.168\.%g|sort -n|grep "*"|sed 's% \*     %%'
+echo "To get to    Last-seen  Quality   Use node    On interface "
+echo "Selected best routes:"
+batctl o | sed s%.-192\.168\.%192\.168\.%g|sort -n|grep "*"|sed 's% \*     %%' | sed "s%\[%%g" | sed "s%\]%%g"
+echo "Other possible routes:"
+batctl o | sed s%.-192\.168\.%192\.168\.%g|sort -n|grep "192\.168\."|grep -v "*"|sed 's%       %%' | sed "s%\[%%g" | sed "s%\]%%g"
 
--- script/bathosts.sh
+++ script/bathosts.sh
@@ -3,15 +3,14 @@
 
 #make /etc/bat-hosts
 alfred -r 66 |sed "s%qqq%\n%g" | grep "{\|HWaddr" > /tmp/alfmacs.txt
-rm /etc/bat-hosts
-touch /etc/bat-hosts
+echo -n "" > /etc/bat-hosts # clear the old file
 while read p; do
   #echo $p
   echo $p | grep { > /dev/null
   if [ $? -eq 0 ]; then
    #mac=`echo "$p" | cut -d " " -f 2 |sed 's%\"%%' | sed 's%\",%%' `
    ip=`echo "$p" | cut -d " " -f 3 |sed 's%\"%%' `
-   ipn=1
+   ipn=97 # will be an "a"
    #echo $mac $ip
   else
    mac=`echo $p | cut -d " " -f 5`
@@ -20,8 +19,11 @@ while read p; do
    if [ $? -eq 0 ]; then
      true
    else
-     echo $mac ${ipn}-${ip} >> /etc/bat-hosts
+     ipnc=`printf '%b' $(printf '\\%03o' $ipn )` # adds the character "a b c ... "
+     echo $mac ${ipnc}-${ip} >> /etc/bat-hosts
      let "ipn=ipn+1"
+     if [ $ipn -gt 122 ]; then ipn=65; fi; #when we run out of lowercase, start using capital A B C ... 
+     if [ $ipn -eq 91 ]; then ipn=97; fi; #don't use special characters, start back with a b c ...
    fi
   fi
 done < /tmp/alfmacs.txt
