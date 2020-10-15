892836f2dff179b0a07f87db4f12a6723af20d67
Andy Haas <drandyhaas@gmail.com>
2020-10-14 22:45:16 -0400

    safer alfredinfo and monstart to prevent loops if alfred fails

--- script/alfredinfo.sh
+++ script/alfredinfo.sh
@@ -6,7 +6,18 @@ while true; do
   echo `/root/script/myip.sh` `echo qqq` `ifconfig |grep HWaddr |sed s%$%qqq%g` | alfred -s 66
   for w in `iwinfo |grep ESSID|cut -d " " -f 1`; do echo -n "$w = "; echo -n `iwinfo $w info | grep Channel`; echo -n " = "; echo `iwinfo $w assoclist | grep SNR | grep dBm | cut -d / -f 1`; done | alfred -s 67
  else
-  /root/script/monstart.sh
+  #restart alfred and batadv-vis
+  killall alfred
+  killall batadv-vis
+  kill `ps|grep alf|grep -v grep|cut -d r -f 1`
+  if [ `/root/script/myip.sh | cut -f 4 -d . ` -eq 1 ]; then
+    echo "master hub"
+    ALFREDMASTER="-m "
+  else
+    echo "just a node"
+  fi
+  alfred $ALFREDMASTER -i br-lan &> /dev/null &  # and -m for master on hub
+  batadv-vis -i bat0 -s &> /root/batadv-vis.log &
  fi
  sleep 30
  /root/script/bathosts.sh
--- script/monstart.sh
+++ script/monstart.sh
@@ -1,21 +1,18 @@
 
-killall alfred
-killall batadv-vis
-kill `ps|grep alf|grep -v grep|cut -d r -f 1`
-
-if [ `/root/script/myip.sh | cut -f 4 -d . ` -eq 1 ]; then
-  echo "master hub"
-  ALFREDMASTER="-m "
+if [ `ps|grep -c alfredinfo` -eq 2 ]; then
+  echo "alfredinfo already running"
 else
-  echo "just a node"
+  echo "start alfredinfo"
+  /root/script/alfredinfo.sh &> /root/alfredinfo.log &
 fi
-
-alfred $ALFREDMASTER -i br-lan &> /dev/null &  # and -m for master on hub
-batadv-vis -i bat0 -s &> /root/batadv-vis.log &
-/root/script/alfredinfo.sh &> /root/alfredinfo.log &
 
 if [ `/root/script/myip.sh | cut -f 4 -d . ` -eq 19 ]; then
   echo "Im a newbie, looking for a parent"
-  /root/script/lookforparent.sh &> /root/lookforparent.log &
+  if [ `ps|grep -c lookforparent` -eq 2 ]; then
+    echo "lookforparent already running"
+  else
+    echo "start lookforparent"
+    /root/script/lookforparent.sh &> /root/lookforparent.log &
+  fi
 fi
 
