b11de81b116334d079ca5aa89f2805dc9b2e27a4
Andy Haas <drandyhaas@gmail.com>
2020-10-19 09:06:19 -0400

    4 threads

--- script/speed.sh
+++ script/speed.sh
@@ -1,4 +1,5 @@
-x=`iperf3 $2 -t 1 -J -c $1 | grep bits_per_second | tail -1 | cut -f 2 -d ':' | cut -f 1 -d '.' | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//'`
+#4 threads, 1 second, json output
+x=`iperf3 $2 -P 4 -t 1 -J -c $1 | grep bits_per_second | tail -1 | cut -f 2 -d ':' | cut -f 1 -d '.' | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//'`
 echo -n " "
 echo -n `echo "print(string.format(\"%.2f\",$x/1000000))" |lua`
 echo " Mb/s"
