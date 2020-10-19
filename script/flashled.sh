871468cd5afb166e06fab22fb777e2e960c77d63
Andy Haas <drandyhaas@gmail.com>
2020-10-19 12:32:09 -0400

    on startup flash once

--- script/flashled.sh
+++ script/flashled.sh
@@ -21,13 +21,21 @@ fi
 if dmesg|grep -q "COMFAST CF-EW72"; then
 led=""
 fi
-if dmesg|grep -q "Luma WRTQ-329ACN"; then
-led=""
-fi
 if dmesg|grep -q "Wavlink WL-WN531A6"; then
 led=/sys/class/leds/blue\:wifi2g
 fi
 
+if dmesg|grep -q "Luma WRTQ-329ACN"; then
+led=""
+for i in $(seq 0 $nsec); do
+ i2cset -y 2 0x48 3 5 12 0 i # set luma led ring to solid purple bright
+ sleep 1
+ i2cset -y 2 0x48 3 5 1 0 i # set luma led ring to solid purple dim
+ sleep 1
+done
+fi
+
+if [ led != "" ]; then
 echo none > $led/trigger
 for i in $(seq 0 $nsec); do
  echo 1 > $led/brightness
@@ -35,4 +43,4 @@ for i in $(seq 0 $nsec); do
  echo 0 > $led/brightness
  sleep 1
 done
-
+fi
