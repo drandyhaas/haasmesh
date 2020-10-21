b46e943a23a6e49ae8b9d4b8d8afddd887e4cec7
Andy Haas <drandyhaas@gmail.com>
2020-10-21 16:51:00 -0400

    can flash led on self

--- script/flashlednode.sh
+++ script/flashlednode.sh
@@ -1,8 +1,11 @@
-echo "<pre>"
 if [ `/root/script/myip.sh | cut -f 4 -d . ` -eq 1 ]; then
-  echo "Will flash node $1 "
-  ssh 192.168.2.$1 "/root/script/flashled.sh"
+  echo "Will flash LED on node $1 via ssh"
+  ssh 192.168.2.$1 "/root/script/flashled.sh "
 else
-  echo "Flash the LED from the master hub, not the node itself"
+  if [ `/root/script/myip.sh | cut -f 4 -d . ` -eq $1 ]; then
+    echo "Will flash LED on myself"
+    /root/script/flashled.sh
+  else
+    echo "Flash the LED on another node from the master hub or the node itself"
+  fi
 fi
-echo "</pre>"
