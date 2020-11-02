6aa6058688998bbc900b3d357830f47c39ec536b
Andy Haas <drandyhaas@gmail.com>
2020-11-01 20:34:48 -0500

    grep right

--- script/rotatelumaeth.sh
+++ script/rotatelumaeth.sh
@@ -1,4 +1,4 @@
-if cat /sys/firmware/devicetree/base/model | grep -q "Machine model: Luma";  then 
+if cat /sys/firmware/devicetree/base/model | grep -q "Luma WRTQ-329ACN";  then 
 echo "On a Luma - rotating eth ports"
 
 cd /etc/config
