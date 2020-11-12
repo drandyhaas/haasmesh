72b60ed2c51366d743c29fe65113f787be288790
Andy Haas <drandyhaas@gmail.com>
2020-11-12 10:55:29 -0500

    need quote

--- script/lookforparent.sh
+++ script/lookforparent.sh
@@ -85,8 +85,8 @@ echo "fixing backhaul mac address(es)" # to stop "received packet on bat0 with o
 batethmac=`ifconfig | grep HW | grep ^\`uci get network.bat_eth.ifname\` |cut -d ":" -f 3-|sed 's% %%g'`
 uci set network.bat_eth.macaddr="00:${batethmac}"
 if /root/script/model.sh |grep -q "Wavlink WL-WN531A6"; then
- uci set network.d_lan1.macaddr="00:${batethmac}
- uci set network.d_lan2.macaddr="00:${batethmac}
+ uci set network.d_lan1.macaddr="00:${batethmac}"
+ uci set network.d_lan2.macaddr="00:${batethmac}"
 fi
 
 echo "setting hostname"
