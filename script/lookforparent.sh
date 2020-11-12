a319a88a7592cd052bcbf57c79e74d490deea783
Andy Haas <drandyhaas@gmail.com>
2020-11-12 08:55:58 -0500

    working now hopefully

--- script/lookforparent.sh
+++ script/lookforparent.sh
@@ -84,6 +84,10 @@ done # looping until we find a parent
 echo "fixing backhaul mac address(es)" # to stop "received packet on bat0 with own address as source address" warnings
 batethmac=`ifconfig | grep HW | grep ^\`uci get network.bat_eth.ifname\` |cut -d ":" -f 3-|sed 's% %%g'`
 uci set network.bat_eth.macaddr="00:${batethmac}"
+if /root/script/model.sh |grep -q "Wavlink WL-WN531A6"; then
+ uci set network.d_lan1.macaddr="00:${batethmac}
+ uci set network.d_lan2.macaddr="00:${batethmac}
+fi
 
 echo "setting hostname"
 myipshort=`echo $myip | cut -f 4 -d . `
