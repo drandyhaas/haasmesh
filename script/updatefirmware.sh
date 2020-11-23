6c148d28ee5ff37f4214695840972ffe397c6621
Andy Haas <drandyhaas@gmail.com>
2020-11-22 19:41:16 -0500

    sysupgrade date for c7/a7 and ew72

--- script/updatefirmware.sh
+++ script/updatefirmware.sh
@@ -13,17 +13,27 @@ if cat /sys/firmware/devicetree/base/model |grep -q "TP-Link Archer C7 v2"; then
 echo "Getting Archer C7 v2 firmware"
 filedir="https://www.dropbox.com/s/90ofp9s7llyrlxr"
 file="openwrt-ath79-generic-tplink_archer-c7-v2-squashfs-sysupgrade.bin"
+vfiledir="https://www.dropbox.com/s/un1zbih4952v68k/"
+vfile="openwrt-ath79-generic-tplink_archer-c7-v2-squashfs-sysupgrade.version.txt"
+docheck=1
 fi
 if cat /sys/firmware/devicetree/base/model |grep -q "TP-Link Archer C7 v5"; then
 echo "Getting Archer C7 v5 firmware"
 filedir="https://www.dropbox.com/s/hb4qmjlduopgwyh"
 file="openwrt-ath79-generic-tplink_archer-c7-v5-squashfs-sysupgrade.bin"
+vfiledir="https://www.dropbox.com/s/nhvnqkwj8jt3vxg/"
+vfile="openwrt-ath79-generic-tplink_archer-c7-v5-squashfs-sysupgrade.version.txt"
+docheck=1
 fi
 if cat /sys/firmware/devicetree/base/model |grep -q "TP-Link Archer A7 v5"; then
 echo "Getting Archer A7 v5 firmware"
 filedir="https://www.dropbox.com/s/cs309z2h3mo46hn"
 file="openwrt-ath79-generic-tplink_archer-a7-v5-squashfs-sysupgrade.bin"
+vfiledir="https://www.dropbox.com/s/7kf8h836ovn00ts/"
+vfile="openwrt-ath79-generic-tplink_archer-a7-v5-squashfs-sysupgrade.version.txt"
+docheck=1
 fi
+
 if cat /sys/firmware/devicetree/base/model |grep -q "Linksys EA8300"; then
 echo "Getting Linksys EA8300 firmware"
 filedir="https://www.dropbox.com/s/n5q1o8bd6pezaic"
@@ -32,11 +42,16 @@ vfiledir="https://www.dropbox.com/s/yw6isf476o872ds/"
 vfile="openwrt-ipq40xx-generic-linksys_ea8300-squashfs-sysupgrade.version.txt"
 docheck=1
 fi
+
 if cat /sys/firmware/devicetree/base/model |grep -q "COMFAST CF-EW72"; then
 echo "Getting COMFAST EW72 firmware"
 filedir="https://www.dropbox.com/s/o3zmy96wxjp4v5m/"
 file="openwrt-ath79-generic-comfast_cf-ew72-squashfs-sysupgrade.bin"
+vfiledir="https://www.dropbox.com/s/jw48vook3cudl35/"
+vfile="openwrt-ath79-generic-comfast_cf-ew72-squashfs-sysupgrade.version.txt"
+docheck=1
 fi
+
 if cat /sys/firmware/devicetree/base/model | grep -q "Compex WPJ419 Haas"; then
 filedir="https://www.dropbox.com/s/wzu97fbm5hlams3/"
 file="openwrt-ipq40xx-generic-compex_wpj419-squashfs-nand-sysupgrade.bin"
@@ -44,6 +59,7 @@ vfiledir="https://www.dropbox.com/s/zt19o7uk0iztotj/"
 vfile="openwrt-ipq40xx-generic-compex_wpj419-squashfs-nand-sysupgrade.version.txt"
 docheck=1
 fi
+
 if cat /sys/firmware/devicetree/base/model |grep -q "WRTQ-329ACN"; then
 echo "Getting Luma WRTQ-329ACN firmware"
 filedir="https://www.dropbox.com/s/ma18jja6zi5na8a"
@@ -52,6 +68,7 @@ vfiledir="https://www.dropbox.com/s/6ge8gvnx0cl3ciz/"
 vfile="openwrt-ipq40xx-generic-luma_wrtq-329acn-squashfs-sysupgrade.version.txt"
 docheck=1
 fi
+
 if cat /sys/firmware/devicetree/base/model |grep -q "Xiaomi Redmi Router AC2100"; then
 echo "Checking for new Xiaomi Redmi Router AC2100 firmware"
 filedir="https://www.dropbox.com/s/dzkgrcm3cgdzkzf"
@@ -60,7 +77,6 @@ vfiledir="https://www.dropbox.com/s/p5jgqgnek75pb19/"
 vfile="openwrt-ramips-mt7621-xiaomi_redmi-router-ac2100-squashfs-sysupgrade.version.txt"
 docheck=1
 fi
-
 if cat /sys/firmware/devicetree/base/model |grep -q "Wavlink WL-WN531A6"; then
 echo "Checking for new Wavlink WL-WN531A6 firmware"
 filedir="https://www.dropbox.com/s/aumhhccy9qbdfxs"
